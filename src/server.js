1fd4888b98baebddec2bbd91ee9ffbdc0ecf5b82
kimgenie0409 <kimgenie0409@gmail.com>
2023-02-25 23:15:11 +0900

    [Update] Basic Camera Usage By JS

--- src/server.js
+++ src/server.js
@@ -12,76 +12,9 @@ app.get('/', (_, res) => res.render("home"));
 app.get('/*', (_, res) => res.redirect('/'));
 
 
-const handleListen = () => console.log("Listen on http://localhost:3000");
 
 const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
-function publicRooms(){
-    const {
-        sockets: {
-            adapter: {sids, rooms}
-        }
-    } = wsServer;
-    const publicRooms = [];
-    rooms.forEach((_, key) => {
-        if(sids.get(key) === undefined){
-            publicRooms.push(key);
-        }
-    });
-    return publicRooms;
-}
-
-function countRoom(roomName){
-    return wsServer.sockets.adapter.rooms.get(roomName)?.size;
-}
-
-wsServer.on("connection", socket => {
-    socket["nickname"] = "Anon";
-    socket.onAny((event) => {
-        console.log(`Socket Event:${event}`);
-    })
-    socket.on("enter_room", (roomName, done) => {
-        socket.join(roomName);
-        done();
-        socket.to(roomName).emit("welcome", socket.nicknam, countRoom(roomName));
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("disconnecting", () => {
-        socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname, countRoom(room)-1));
-    });
-    socket.on("disconnect", ()=> {
-        wsServer.sockets.emit("room_change", publicRooms());
-    });
-    socket.on("new_message", (msg, room, done) => {
-        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
-        done();
-    });
-
-    socket.on("nickname", (nickname) => (socket['nickname']=nickname));
-})
-
-// const wss = new WebSocket.Server({ server });
-// const sockets = [];
-
-// wss.on("connection", (socket) => {
-//     sockets.push(socket);
-//     socket["nickname"] = "Anon";
-//     console.log("Connected to Browser 🟩");
-//     socket.on("close", () => console.log("Disconnected From the Browser 🟥"));
-//     socket.on("message", (msg) => {
-//         const message = JSON.parse(msg.toString('utf8'));
-//         switch (message.type){
-//             case "new_message":
-//                 sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
-//                 break;
-//             case "nickname":
-//                 socket["nickname"] = message.payload;
-//                 break;
-//             default:
-//                 console.log("message error");
-//         }
-//     });
-// });
-
+const handleListen = () => console.log("Listen on http://localhost:3000");
 httpServer.listen(3000, handleListen);
